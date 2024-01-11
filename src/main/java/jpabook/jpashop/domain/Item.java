e8c0297e275ae108c5a1537f8a6868e70ddd3220
NamTaeHo <han127kr@gmail.com>
2024-01-11 18:18:01 +0900

    요구사항 분석과 기본 매핑

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,4 +1,50 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+    private String name;
+    private int prive;
+    private int stockQuantity;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrive(int prive) {
+        this.prive = prive;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrive() {
+        return prive;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
 }
